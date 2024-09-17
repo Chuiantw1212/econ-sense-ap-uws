e3e8fe2d1a8741fd65cdd7ff1da48996f9d4d119
Chuian <chuiantw1212@gmail.com>
2024-09-17 16:13:32 +0800

    wip

--- adapters/blog.in/root.ctrl.ts
+++ adapters/blog.in/root.ctrl.ts
@@ -1,28 +1,30 @@
 import uWS from 'uWebSockets.js'
 import { memoryUsage } from 'node:process'
 import { ILocals } from '../../entities/app'
-const rootController = new HyperExpress.Router()
-rootController.get('/', async (request, response) => {
-    const memoryUsageInMB: ReturnType<typeof memoryUsage> = {
-        rss: 0,
-        heapTotal: 0,
-        heapUsed: 0,
-        arrayBuffers: 0,
-        external: 0,
-    }
-    const currentMemoryUsage: any = memoryUsage()
-    for (const key in memoryUsageInMB) {
-        const mb: number = Math.floor(1024 * 1024)
-        const value: number = currentMemoryUsage[key]
-        const valueInMB: number = Math.floor(value / mb)
-        Object.assign(memoryUsageInMB, {
-            [key]: `${valueInMB.toLocaleString()}Mb`
-        })
-    }
-    const locals = request.app.locals as ILocals
-    response.json({
-        memoryUsage: memoryUsageInMB,
-        startupTime: `${locals.startupTime}s`,
+
+export default function (App: ReturnType<typeof uWS.App>) {
+    App.get('/', async (res, req) => {
+        const memoryUsageInMB: ReturnType<typeof memoryUsage> = {
+            rss: 0,
+            heapTotal: 0,
+            heapUsed: 0,
+            arrayBuffers: 0,
+            external: 0,
+        }
+        const currentMemoryUsage: any = memoryUsage()
+        for (const key in memoryUsageInMB) {
+            const mb: number = Math.floor(1024 * 1024)
+            const value: number = currentMemoryUsage[key]
+            const valueInMB: number = Math.floor(value / mb)
+            Object.assign(memoryUsageInMB, {
+                [key]: `${valueInMB.toLocaleString()}Mb`
+            })
+        }
+        // const locals = req.app.locals as ILocals
+        const serverStart = {
+            memoryUsage: memoryUsageInMB,
+            // startupTime: `${locals.startupTime}s`,
+        }
+        res.end(serverStart)
     })
-})
-export default rootController
+}
--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
+/**
+ * 官方說明文件
+ * https://github.com/uNetworking/uWebSockets.js/tree/master/examples
+ * 
+ * In Memory Cached
+ * https://github.com/uNetworking/uWebSockets.js/blob/master/examples/CachedHelloWorld.js
+ * 
+ * Parse JSON Body
+ * https://github.com/uNetworking/uWebSockets.js/blob/master/examples/JsonPost.js
+ * 
+ * 路由範例
+ * https://github.com/uNetworking/uWebSockets.js/blob/master/examples/Router.js
+ */
 // 效能計時開始
 const time = new Date().getTime()
 import uWS from 'uWebSockets.js'
@@ -7,19 +20,22 @@ import googleCloud from './adapters/googleCloud.out'
 import chatGpt from './adapters/chatGpt.out'
 import centralBank from './adapters/centralBank.out'
 import ishares from './adapters/ishares.out'
-// 
-const port = 9001;
+// controllers
+import rootController from './adapters/blog.in/root.ctrl'
 
-const app = uWS./*SSL*/App({
-    key_file_name: 'misc/key.pem',
-    cert_file_name: 'misc/cert.pem',
-    passphrase: '1234'
-}).get('/*', (res) => {
-    res.end('Hello World!');
-}).listen(port, (token) => {
-    if (token) {
-        console.log('Listening to port ' + port);
-    } else {
-        console.log('Failed to listen to port ' + port);
-    }
-});
+(async () => {
+    const port = 9001;
+    global.test = ''
+    const app = uWS.App()
+    app.get('/*', (res) => {
+        res.end('Hello World!');
+    })
+
+    app.listen(port, (token) => {
+        if (token) {
+            console.log('Listening to port ' + port);
+        } else {
+            console.log('Failed to listen to port ' + port);
+        }
+    });
+})()
